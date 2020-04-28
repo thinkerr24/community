956713222aa8115cdfd15be2307f887dcf30de02
thinkerr24 <thinkerr24@163.com>
2020-04-28 22:14:52 +0800

    1.2.2 Use applicationContext to get dao-bean.

--- src/test/java/com/nowcoder/community/CommunityApplicationTests.java
+++ src/test/java/com/nowcoder/community/CommunityApplicationTests.java
@@ -1,7 +1,11 @@
 package com.nowcoder.community;
 
+import com.nowcoder.community.controller.AlphaController;
+import com.nowcoder.community.dao.AlphaDao;
+import com.nowcoder.community.dao.AlphaDaoHibernateImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
@@ -23,6 +27,15 @@ class CommunityApplicationTests implements ApplicationContextAware {
 	// Test Spring container
 	@Test
 	public void testApplicationContext() {
-		System.out.println(applicationContext);
+		// System.out.println(applicationContext);
+
+		// Single implement class
+		AlphaDao alphaDao = applicationContext.getBean(AlphaDao.class);
+		System.out.println(alphaDao.select());
+
+		// Get specific bean
+		// AlphaDao alphaDao2 = (AlphaDaoHibernateImpl)applicationContext.getBean("alphaHibernate"); or
+		AlphaDao alphaDao2 = applicationContext.getBean("alphaHibernate", AlphaDao.class);
+		System.out.println(alphaDao2.select());
 	}
 }
