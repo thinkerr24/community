783a56ef9ea7bbb3a1b3bff33f254c138ef0a423
thinkerr24 <thinkerr24@163.com>
2020-04-28 22:56:29 +0800

    1.2.3  applicationContext singleton bean.

--- src/test/java/com/nowcoder/community/CommunityApplicationTests.java
+++ src/test/java/com/nowcoder/community/CommunityApplicationTests.java
@@ -7,10 +7,14 @@ import com.nowcoder.community.service.AlphaService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.BeansException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.test.context.ContextConfiguration;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 @SpringBootTest
 @ContextConfiguration(classes = CommunityApplication.class)
@@ -49,5 +53,29 @@ class CommunityApplicationTests implements ApplicationContextAware {
 
 		AlphaService alphaService2 = applicationContext.getBean(AlphaService.class);
 		System.out.println(alphaService == alphaService2);  // when scope is prototype, it is false
+	}
+
+	// Get bean(third party class) from applicationContext
+	@Test
+	public void testBeanConfig() {
+		SimpleDateFormat simpleDateFormat = applicationContext.getBean(SimpleDateFormat.class);
+		System.out.println(simpleDateFormat.format(new Date()));
+	}
+
+	// Get bean by annotation
+	@Autowired
+	public AlphaDao dao;
+
+	@Autowired
+	@Qualifier("alphaHibernate")
+	public AlphaDao dao2;
+
+	@Autowired
+	public SimpleDateFormat simpleDateFormat;
+	@Test
+	public void testDI() {
+		System.out.println(dao.select());
+		System.out.println(dao2.select());
+		System.out.println(simpleDateFormat.format(new Date()));
 	}
 }
