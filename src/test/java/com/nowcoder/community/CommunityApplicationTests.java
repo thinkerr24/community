2b81fd0992c019e3681e4b34155a193f0b8f8381
thinkerr24 <thinkerr24@163.com>
2020-04-28 23:02:44 +0800

    1.2.4  DI: Inject third-party bean.

--- src/test/java/com/nowcoder/community/CommunityApplicationTests.java
+++ src/test/java/com/nowcoder/community/CommunityApplicationTests.java
@@ -54,29 +54,5 @@ class CommunityApplicationTests implements ApplicationContextAware {
 		AlphaService alphaService2 = applicationContext.getBean(AlphaService.class);
 		System.out.println(alphaService == alphaService2);  // when scope is prototype, it is false
 	}
-
-	// Get bean(third party class) from applicationContext
-	@Test
-	public void testBeanConfig() {
-		SimpleDateFormat simpleDateFormat = applicationContext.getBean(SimpleDateFormat.class);
-		System.out.println(simpleDateFormat.format(new Date()));
-	}
-
-	// Get bean by annotation
-	@Autowired
-	public AlphaDao dao;
-
-	@Autowired
-	@Qualifier("alphaHibernate")
-	public AlphaDao dao2;
-
-	@Autowired
-	public SimpleDateFormat simpleDateFormat;
 	
-	@Test
-	public void testDI() {
-		System.out.println(dao.select());
-		System.out.println(dao2.select());
-		System.out.println(simpleDateFormat.format(new Date()));
-	}
 }
