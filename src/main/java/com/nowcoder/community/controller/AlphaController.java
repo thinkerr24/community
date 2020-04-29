eed94adde3ce08a56ef9eeb6d45891a7f8a21369
thinkerr24 <thinkerr24@163.com>
2020-04-29 23:30:01 +0800

    1.3.1  Http protocol: simple use of res&req obj.

--- src/main/java/com/nowcoder/community/controller/AlphaController.java
+++ src/main/java/com/nowcoder/community/controller/AlphaController.java
@@ -6,6 +6,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Enumeration;
+
 /**
  *  Simple Contoller functional test
  */
@@ -27,6 +33,31 @@ public class AlphaController {
     @ResponseBody
     public String getData() {
         return service.find();
+    }
+
+    // Example of request&response in Controller, access url:http://localhost:8080/community/alpha/http?code=77r
+    @RequestMapping("/http")
+    public void http(HttpServletRequest req, HttpServletResponse res) {
+        // Get request data
+        System.out.println(req.getMethod());
+        System.out.println(req.getServletPath());
+        Enumeration<String> enumeration = req.getHeaderNames();
+        while (enumeration.hasMoreElements()) {
+            String name = enumeration.nextElement();
+            String value = req.getHeader(name);
+            System.out.println(name + ":" + value);
+        }
+        System.out.println(req.getParameter("code"));
+
+        // return response data
+        res.setContentType("text/html;charset=utf-8");
+        try( PrintWriter writer = res.getWriter();
+        ) {
+
+            writer.write("<h1>Welcome learn Spring Boot!</h1>");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
 
