9071f637edbb34b12ce615658c74184c074e24a6
thinkerr24 <thinkerr24@163.com>
2020-06-05 00:03:22 +0800

    2.1.4 Activate registered account.

--- src/main/java/com/nowcoder/community/controller/LoginController.java
+++ src/main/java/com/nowcoder/community/controller/LoginController.java
@@ -2,20 +2,24 @@ package com.nowcoder.community.controller;
 
 import com.nowcoder.community.entity.User;
 import com.nowcoder.community.service.UserService;
+import com.nowcoder.community.util.CommunityConstant;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
 @Controller
-public class LoginController {
+public class LoginController implements CommunityConstant {
 
     @Autowired
     private UserService userService;
+
+    @RequestMapping(path = "/login", method = RequestMethod.GET)
+    public String getLoginPage() {
+        return "/site/login";
+    }
 
     @RequestMapping(path = "/register", method = RequestMethod.GET)
     public String getRegisterPage() {
@@ -35,5 +39,22 @@ public class LoginController {
            model.addAttribute("emailMsg", map.get("emailMsg"));
            return "/site/register";
        }
+    }
+
+    // http://localhost:8080/community/activation/101/code
+    @GetMapping(path = "/activation/{userId}/{code}")
+    public String activation(Model model, @PathVariable("userId") int userId, @PathVariable("code") String code) {
+        int result = userService.activation(userId, code);
+        if (result == ACTIVATION_SUCCESS) {
+            model.addAttribute("msg", "激活成功, 您的账号可正常使用了!");
+            model.addAttribute("target", "/login");
+        } else if (result == ACTIVATION_REPEAT) {
+            model.addAttribute("msg", "无效操作, 该账号已经激活过了!");
+            model.addAttribute("target", "/index");
+        } else {
+            model.addAttribute("msg", "激活失败，您提供的激活码不正确!");
+            model.addAttribute("target", "/index");
+        }
+        return "/site/operate-result";
     }
 }
