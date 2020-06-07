306d5c459fab95a5a2dea562ad9e0d194ce65361
thinkerr24 <thinkerr24@163.com>
2020-06-07 22:27:38 +0800

    2.3.2 show loginInformation by interceptor.

---
+++ src/main/java/com/nowcoder/community/util/CookieUtil.java
@@ -0,0 +1,26 @@
+package com.nowcoder.community.util;
+
+import org.apache.commons.lang3.StringUtils;
+
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+
+public class CookieUtil {
+
+    public static String getValue(HttpServletRequest req, String name) {
+
+        if (req == null || StringUtils.isBlank(name)) {
+            throw new IllegalArgumentException("参数为空！");
+        }
+
+        Cookie[] cookies = req.getCookies();
+        if (cookies != null) {
+            for (Cookie cookie: cookies) {
+                if (cookie.getName().equals(name)) {
+                    return cookie.getValue();
+                }
+            }
+        }
+        return  null;
+    }
+}
