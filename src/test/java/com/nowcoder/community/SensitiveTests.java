84fecdc8c93b9d001b0d0c5e75bacec947baa9c2
thinkerr24 <thinkerr24@163.com>
2020-06-10 07:52:54 +0800

    3.1.1 Filtering sensitive words by Trie.

---
+++ src/test/java/com/nowcoder/community/SensitiveTests.java
@@ -0,0 +1,23 @@
+package com.nowcoder.community;
+
+import com.nowcoder.community.util.SensitiveFilter;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+
+
+@SpringBootTest
+@ContextConfiguration(classes = CommunityApplication.class)
+public class SensitiveTests {
+
+    @Autowired
+    private SensitiveFilter sensitiveFilter;
+
+    @Test
+    public void testSensitiveFilter() {
+        String text = "这里可以⭐赌⭐博⭐，可以嫖♂娼，可以⭐吸毒，可以开○票, 哈哈哈!";
+        System.out.println(sensitiveFilter.filter(text));
+    }
+}
