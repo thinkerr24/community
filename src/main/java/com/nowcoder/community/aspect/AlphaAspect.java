785c00f770323f706a8ecd6e2a5a10ff13d03d1b
thinkerr24 <thinkerr24@163.com>
2020-06-17 09:53:12 +0800

    3.9.3 Handle logs uniformly-Aop ends.

--- src/main/java/com/nowcoder/community/aspect/AlphaAspect.java
+++ src/main/java/com/nowcoder/community/aspect/AlphaAspect.java
@@ -2,10 +2,10 @@ package com.nowcoder.community.aspect;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.*;
-import org.springframework.stereotype.Component;
+//import org.springframework.stereotype.Component;
 
-@Component
-@Aspect
+/*@Component
+@Aspect*/
 public class AlphaAspect {
 
     @Pointcut("execution(* com.nowcoder.community.service.*.*(..))")
