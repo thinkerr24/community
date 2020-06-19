dc97f317eb3af032367be2b9c17df044a6c51518
thinkerr24 <thinkerr24@163.com>
2020-06-19 11:17:49 +0800

    4.3.1 Redis likeCount&Status.

---
+++ src/main/java/com/nowcoder/community/util/RedisUtil.java
@@ -0,0 +1,14 @@
+package com.nowcoder.community.util;
+
+public class RedisUtil {
+
+    private static final String SPLIT = ":";
+    private static final String PREFIX_ENTITY_LIKE = "like:entity";
+
+    // 某个实体的赞
+    // like:entity:entityType:entityId -> set(userId)
+    public static String getEntityLikeKey(int entityType, int entityId) {
+        return PREFIX_ENTITY_LIKE + entityType  + SPLIT + entityId;
+    }
+
+}
