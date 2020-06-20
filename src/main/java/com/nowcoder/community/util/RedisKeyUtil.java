9281dfb32b049c5e1936e6b72de3ba22ee4408dd
thinkerr24 <thinkerr24@163.com>
2020-06-20 15:44:51 +0800

    4.4.1 The Likes I received.

--- src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
+++ src/main/java/com/nowcoder/community/util/RedisKeyUtil.java
@@ -1,9 +1,10 @@
 package com.nowcoder.community.util;
 
-public class RedisUtil {
+public class RedisKeyUtil {
 
     private static final String SPLIT = ":";
     private static final String PREFIX_ENTITY_LIKE = "like:entity";
+    private static final String PREFIX_USER_LIKE = "like:user";
 
     // 某个实体的赞
     // like:entity:entityType:entityId -> set(userId)
@@ -11,4 +12,9 @@ public class RedisUtil {
         return PREFIX_ENTITY_LIKE + entityType  + SPLIT + entityId;
     }
 
+    // 某个用户的赞
+    // like:user:userId -> int
+    public static String getUserLikeKey(int userId) {
+        return PREFIX_USER_LIKE + SPLIT + userId;
+    }
 }
