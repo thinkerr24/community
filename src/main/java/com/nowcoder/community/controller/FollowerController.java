771e0696e634bad193016cbb52494f95f8cff0e0
thinkerr24 <thinkerr24@163.com>
2020-06-20 20:51:33 +0800

    4.5.1 Follow & unFollow.

---
+++ src/main/java/com/nowcoder/community/controller/FollowerController.java
@@ -0,0 +1,42 @@
+package com.nowcoder.community.controller;
+
+import com.nowcoder.community.entity.User;
+import com.nowcoder.community.service.FollowerService;
+import com.nowcoder.community.util.CommunityUtil;
+import com.nowcoder.community.util.HostHolder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class FollowerController {
+
+    @Autowired
+    private FollowerService followerService;
+
+    @Autowired
+    private HostHolder hostHolder;
+
+    @RequestMapping(path = "/follow", method = RequestMethod.POST)
+    @ResponseBody
+    public String follow(int entityType, int entityId) {
+        User user = hostHolder.getUser();
+
+        followerService.follow(user.getId(), entityType, entityId);
+
+        return CommunityUtil.getJSONString(0, "已关注");
+    }
+
+
+    @RequestMapping(path = "/unfollow", method = RequestMethod.POST)
+    @ResponseBody
+    public String unFollow(int entityType, int entityId) {
+        User user = hostHolder.getUser();
+
+        followerService.unFollow(user.getId(), entityType, entityId);
+
+        return CommunityUtil.getJSONString(0, "已取消关注");
+    }
+}
