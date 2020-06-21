c317f96e5a3c604a80b72e55edfd41f3ea0756f7
thinkerr24 <thinkerr24@163.com>
2020-06-21 10:59:26 +0800

    5.1.2 Kafka cmd quickstart.

--- src/test/java/com/nowcoder/community/BlockingQueueTests.java
+++ src/test/java/com/nowcoder/community/BlockingQueueTests.java
@@ -58,3 +58,27 @@ class Consumer implements Runnable {
         }
     }
 }
+
+/* Note：Kafka在Win10下简单使用:
+   1. Dowload:http://kafka.apache.org/downloads
+   2. 解压后配置:
+      afka_2.12-2.2.0\config\zookeeper.properties: dataDir=D:/work/data/zookeeper
+      kafka_2.12-2.2.0\config\server.properties  : log.dirs=D:/work/data/kafka-logs
+   3. 启动:
+ new cmd   cd kafka_2.12-2.2.0 dir
+          >bin\windows\zookeeper-server-start.bat config\zookeeper.properties
+ new cmd: >bin\windows\kafka-server-start.bat config\server.properties
+          观察上面配置的目录是被创建并有新文件产生
+ new cmd: cd kafka_2.12-2.2.0\bin\windows                                (默认端口)                （创建1个副本）  （1个分区） (topic名字)
+          创建主题:   kafka-topics.bat --create --bootstrap-server localhost:9092 --replication-factor 1 --partitions 1 --topic test
+          查看主题:   kafka-topics.bat --list --bootstrap-server localhost:9092
+    4.使用:
+        生产者发送消息:
+        kafka-console-producer.bat --broker-list localhost:9092 --topic test
+        >hello
+        >world
+        >
+ new cmd: cd kafka_2.12-2.2.0\bin\windows
+          kafka-console-consumer.bat --bootstrap-server localhost:9092 -topic test --from-beginning
+
+ */
