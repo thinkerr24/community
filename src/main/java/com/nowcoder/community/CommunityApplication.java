248662a2e7d6a30690fbec276f2e18abc0e945c9
thinkerr24 <thinkerr24@163.com>
2020-06-25 10:29:11 +0800

    6.2.1 ElasticSearch in Spring basic configuration & Solve Netty conflict.

--- src/main/java/com/nowcoder/community/CommunityApplication.java
+++ src/main/java/com/nowcoder/community/CommunityApplication.java
@@ -3,8 +3,16 @@ package com.nowcoder.community;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PostConstruct;
+
 @SpringBootApplication
 public class CommunityApplication {
+
+	@PostConstruct
+	public void init() {
+		// 解决netty启动冲突(redis和ElasticSearch)的问题 参考NettyRuntime&Netty4Util类的setAvailableProcessors方法
+		System.setProperty("es.set.netty.runtime.available.processors", "false");
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
 	}
